ade8092db527d0657d44b3393a523a307daf4d5b
LucasPaulAltamizaJulian <q01101g@upla.edu.pe>
2026-06-25 21:36:38 -0500

    infrasestructura

--- Solution1/AplicacionDBContexto.cs
+++ Solution1/AplicacionDBContexto.cs
@@ -1,8 +1,44 @@
-﻿using System;
+﻿using Dinnacem.Dominio.Entidades;
+using Microsoft.EntityFrameworkCore;
 
-public class Class1
+namespace DinacemInfraestructura.Data
 {
-	public Class1()
-	{
-	}
+    public class AplicacionDbContexto : DbContext
+    {
+        public AplicacionDbContexto(DbContextOptions<AplicacionDbContexto> options)
+            : base(options)
+        {
+        }
+
+        // Seguridad
+        public DbSet<Rol> Roles { get; set; }
+        public DbSet<Usuario> Usuarios { get; set; }
+
+        // Solicitudes
+        public DbSet<EstadoSolicitud> EstadoSolicitudes { get; set; }
+        public DbSet<Solicitud> Solicitudes { get; set; }
+
+        // Rendiciones
+        public DbSet<EstadoRendicion> EstadoRendiciones { get; set; }
+        public DbSet<Rendicion> Rendiciones { get; set; }
+
+        // Catálogos
+        public DbSet<TipoGasto> TipoGastos { get; set; }
+        public DbSet<TipoComprobante> TipoComprobantes { get; set; }
+
+        // Gastos
+        public DbSet<Gasto> Gastos { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            // Evitar eliminación en cascada
+            foreach (var relationship in modelBuilder.Model.GetEntityTypes()
+                         .SelectMany(e => e.GetForeignKeys()))
+            {
+                relationship.DeleteBehavior = DeleteBehavior.Restrict;
+            }
+        }
+    }
 }
